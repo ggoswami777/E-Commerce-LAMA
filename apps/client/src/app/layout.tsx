9fcdbbec7c397f84d18da22764bce410ad677e07
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-04-15 18:08:03 +0530

    added clerk auth

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -37,5 +39,6 @@ export default function RootLayout({
           <ToastContainer position="bottom-right"/>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link"
 import Image from 'next/image'
 import SearchBar from './SearchBar'
 import { Bell, Home, ShoppingCart } from 'lucide-react'
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
 import ShoppingCartIcon from './ShoppingCartIcon'
 
 const Navbar = () => {
@@ -20,7 +22,17 @@ const Navbar = () => {
             <Home className='w-4 h-4 text-gray-500 '/></Link>
             <Bell className='w-4 h-4 text-gray-500 '/>
             <ShoppingCartIcon/>
-            <Link href='/login'>Sign in</Link>
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
         </div>
     </div>
   )
