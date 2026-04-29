b815f4a15360195f24a7b8aef0de808de0e8ebc7
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-04-29 22:47:09 +0530

    refactor: centralize prisma client in product-db and modernize ts config

--- packages/product-db/src/index.ts
+++ packages/product-db/src/index.ts
@@ -1,12 +1,2 @@
-import { PrismaClient } from "./generated/client.js";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
-
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({ adapter });
-
-export * from "./generated/client.js";
+export { prisma } from "./client"; // exports instance of prisma
+export * from "./generated/client"; // exports generated types from prisma
