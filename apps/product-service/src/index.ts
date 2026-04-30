01db0690fc28384615d40821dcf2101e2e051204
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-04-30 17:37:18 +0530

    feat(product-service):added createcategory and createproduct route

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,17 +1,26 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import cors from "cors";
 import { clerkMiddleware, getAuth } from "@clerk/express";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
-
+import productRouter from "./routes/product.route";
+import categoryRouter from "./routes/category.route";
+import { Request,Response } from "express";
 const app=express();
 app.use(cors({
     origin:["https://localhost:3002","http://localhost:3003"],credentials:true,
 }))
+app.use(express.json());
 app.use(clerkMiddleware());
 app.get("/test",shouldBeUser,(req,res)=>{
    
    
     return res.json({message:"Product service authenticated",userId:req.userId})
+})
+app.use('/products',productRouter);
+app.use('/categories',categoryRouter);
+app.use((err:any,req:Request,res:Response,next:NextFunction)=>{
+    console.log(err);
+    return res.status(err.status || 500).json({message:err.message || "Internal Server Error!"})
 })
 app.listen(8000,()=>{
     console.log("product service is running")
