6d52f36032e9acc198f3a63d98cc6aea98863a76
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-01 16:11:13 +0530

    feat(product-service):update and delete product

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -20,10 +20,22 @@ export const createProduct=async(req:Request,res:Response)=>{
 
 }
 export const updateProduct=async(req:Request,res:Response)=>{
-
+    const {id}=req.params;
+    const data:Prisma.ProductUpdateInput=req.body
+    const updatedProduct=await prisma.product.update({
+        where:{id:Number(id)},
+        data,
+    })
+    return res.status(200).json(updatedProduct)
 }
 export const deleteProduct=async(req:Request,res:Response)=>{
-
+    const {id}=req.params;
+    
+    const deletedProduct=await prisma.product.delete({
+        where:{id:Number(id)},
+        
+    })
+    return res.status(200).json(deletedProduct)
 }
 export const getProducts=async(req:Request,res:Response)=>{
     const {sort,category,search,limit}=req.query;
