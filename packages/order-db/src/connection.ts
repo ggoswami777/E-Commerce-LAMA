191abc8bf31a407d2bc2c21c9dc725c184127200
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-01 16:33:52 +0530

    feat(order-db)added mongoose and connect mongodb

---
+++ packages/order-db/src/connection.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+let isConnected=false;
+export const connectOrderDB= async()=>{
+    if(isConnected){
+        return;
+    }
+    if(!process.env.MONGO_URL){
+        throw new Error("MONGO URL is not defined in env file");
+    }
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        isConnected=true;
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
