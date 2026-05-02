6ea85108614ee2919caf01a22e864673fd45671a
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-02 18:02:12 +0530

    feat(product-service):added auth middleware for admin

--- apps/product-service/src/middleware/authMiddleware.ts
+++ apps/product-service/src/middleware/authMiddleware.ts
@@ -1,6 +1,6 @@
 import { getAuth } from "@clerk/express";
 import { Request, Response, NextFunction } from "express";
-
+import {CustomJwtSessionClaims} from "@repo/types"
 declare global{
     namespace Express{
         interface Request{
@@ -14,6 +14,19 @@ export const shouldBeUser = (req: Request, res: Response, next: NextFunction) =>
     const userId = auth.userId;
     if (!userId) {
         return res.status(401).json({ message: "You are not logged in" });
+    }
+    req.userId = userId;
+    return next();
+};
+export const shouldBeAdmin = (req: Request, res: Response, next: NextFunction) => {
+    const auth = getAuth(req);
+    const userId = auth.userId;
+    if (!userId) {
+        return res.status(401).json({ message: "You are not logged in" });
+    }
+    const claims=auth.sessionClaims as CustomJwtSessionClaims
+    if(claims.metadata?.role!=="admin"){
+        return res.status(403).send({message:"Unauthorized!"})
     }
     req.userId = userId;
     return next();
