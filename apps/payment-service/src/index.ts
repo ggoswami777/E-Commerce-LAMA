4a557e03ef6bb125800bfdefd11756e72d395685
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-04 18:25:49 +0530

    added functions which will be later triggered by kafka for stripe

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -2,18 +2,30 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { shouldBeUser } from './middleware/authMiddleware.js'
+import stripe from './utils/stripe.js'
 
 const app = new Hono()
 app.use('*', clerkMiddleware())
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-app.get('/test',shouldBeUser, (c) => {
-  return c.json({
-    message: 'Payment service is authenticated', userId:c.get("userId")
-    
-  })
-})
+// app.post('/create-stripe-product',async (c) => {
+//   const res=await stripe.products.create({
+//     id:"123",
+//     name:"Test Product",
+//     default_price_data:{
+//       currency:"usd",
+//       unit_amount:10*100
+//     },
+//   });
+//   return c.json(res);
+// })
+// app.post('/stripe-product-price',async (c) => {
+//   const res=await stripe.prices.list({
+//     product:"123",
+
+//   })
+// })
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8002;
 
