9c86fda28bf7d80da503ccf845a531f800335daf
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-13 19:21:59 +0530

    set up kafka for order service

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -6,6 +6,7 @@ import sessionRoute from './routes/session.routes.js'
 import {cors} from "hono/cors"
 import webhookRoute from './routes/webhooks.route.js'
 import stripe from './utils/stripe.js'
+import { consumer, producer } from './utils/kafka.js'
 const app = new Hono()
 app.use("*",cors({
   origin:["http://localhost:3002"],
@@ -41,6 +42,7 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 8002;
 
 const start=async()=>{
   try {
+    Promise.all([await producer.connect(),await consumer.connect()])
     serve({
   fetch: app.fetch,
   port: port
