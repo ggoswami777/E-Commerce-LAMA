c04693bb62ff0db1de40530fc7b92f42bb463f0b
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-21 21:08:44 +0530

    search filter functionality0

--- apps/client/src/components/SearchBar.tsx
+++ apps/client/src/components/SearchBar.tsx
@@ -1,10 +1,25 @@
+"use client"
 import { Search } from 'lucide-react'
-import React from 'react'
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useState } from 'react'
 const SearchBar = () => {
+  const [value,setValue]=useState("");
+  const searchParams= useSearchParams();
+  const router=useRouter();
+  const handleSearch=(value:string)=>{
+    const params=new URLSearchParams(searchParams);
+    params.set("search",value);
+    router.push(`/products?${params.toString()}`,{scroll:false})
+  }
   return (
     <div className='hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-200 px-2 py-1 shadow-md'>
         <Search className='w-4 h-4 text-gray-500 '/>
-        <input id="search" placeholder='Search...' className='text-sm outline-0'/>
+        <input id="search" placeholder='Search...' 
+        onChange={e=>setValue(e.target.value)}
+        onKeyDown={e=>{if(e.key==="Enter"){
+          handleSearch(value);
+        }}}
+        className='text-sm outline-0'/>
     </div>
   )
 }
