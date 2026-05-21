e57e635b62e95dab54e287b80b12af16e82a83c5
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-05-21 20:57:15 +0530

    fetching products from db

--- apps/client/src/app/products/page.tsx
+++ apps/client/src/app/products/page.tsx
@@ -1,10 +1,12 @@
 import ProductList from "@/components/ProductList"
 
-const page =  async({searchParams}:{searchParams:Promise<{category:string}>}) => {
+const page =  async({searchParams}:{searchParams:Promise<{category:string,sort:string,search:string}>}) => {
      const category=(await searchParams).category;
+     const sort=(await searchParams).sort;
+     const search=(await searchParams).search;
   return (
     <div>
-        <ProductList category={category} params="products"/>
+        <ProductList category={category} sort={sort} search={search}params="products"/>
     </div>
   )
 }
