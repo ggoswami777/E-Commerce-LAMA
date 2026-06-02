67accce9d4ea2fedafc435d78ca5eaa8a5a99aad
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-06-02 16:45:57 +0530

    feat: initialize order type definitions

--- packages/types/src/order.ts
+++ packages/types/src/order.ts
@@ -2,3 +2,8 @@ import {OrderSchemaType} from "@repo/order-db";
 export type OrderType=OrderSchemaType &{
     _id:string;
 }
+export type OrderChartType={
+    month:string;
+    total:number;
+    successful:number
+}
